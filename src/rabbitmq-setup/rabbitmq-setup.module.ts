784797d2d7c056f8f063e1ac79df36069b921f65
Cristiano Souza <cristiano@fasters.com.br>
2025-05-06 09:02:05 -0300

    ajustando modulo de setup

--- src/rabbitmq-setup/rabbitmq-setup.module.ts
+++ src/rabbitmq-setup/rabbitmq-setup.module.ts
@@ -10,6 +10,14 @@ import { RabbitSetupOptions } from './interfaces';
 })
 export class RabbitmqSetupModule<Q,E,R> {
 
+  /**
+   * Registers the RabbitMQ Setup module with the given options
+   * 
+   * The generic types Q, E and R are used to specify the types of the queue, exchange and routing keys. They can be Enums of strings for type cheching
+   *    
+   * @param options The binding options for the RabbitMQ Setup module @see RabbitSetupOptions
+   * @returns A dynamic module with the RabbitMQ Setup service as a provider and export
+   */
   static register<Q,E,R>(options: RabbitSetupOptions<Q,E,R>): DynamicModule {
     return {
       module: RabbitmqSetupModule,
